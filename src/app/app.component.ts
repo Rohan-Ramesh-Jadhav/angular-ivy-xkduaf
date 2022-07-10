09f76f964822872c395761eedaa6042f7b42005b
Rohan-Ramesh-Jadhav <rohanjdhv550@gmail.com>
2022-07-10 23:02:09 +0530

    3 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  private visiblity: boolean = false;
+
+  Check() {
+    return this.visiblity;
+  }
+
+  toggle() {
+    this.visiblity = !this.visiblity;
+  }
 }
